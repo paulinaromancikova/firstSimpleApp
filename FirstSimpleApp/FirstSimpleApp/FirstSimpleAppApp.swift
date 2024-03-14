1472b073fbeab1bec87c3f1e4a5a7b80ef2cef49
paulinaromancikova <125348036+paulinaromancikova@users.noreply.github.com>
2024-03-14 15:32:22 +0100

    created project

---
+++ FirstSimpleApp/FirstSimpleApp/FirstSimpleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstSimpleAppApp.swift
+//  FirstSimpleApp
+//
+//  Created by Paulína Romančíková on 14/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct FirstSimpleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
